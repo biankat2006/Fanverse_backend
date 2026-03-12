22db5cfb8c01213f34966966f9195de3a2af5c11
diak <diak@mesterlevel.lo>
2026-03-12 11:53:34 +0100

    kifinomultabb kereső és randomizált játék bedobás

--- models/mainModel.js
+++ models/mainModel.js
@@ -2,15 +2,26 @@ const db = require('../db/db')
 
 
 async function findbytitle(title) {
-    const sql = "SELECT title , description , banner_pic , creators.creator_pfp , creators.creator_name FROM games INNER JOIN creators ON games.creator_id =creators.creator_id  WHERE title LIKE ?"
-    const [result] = await db.query(sql, [`%${title}%`])
+    const sql =`
+        SELECT 
+            games.title, 
+            games.description, 
+            games.banner_pic, 
+            creators.creator_pfp, 
+            creators.creator_name
+        FROM games
+        INNER JOIN creators ON games.creator_id = creators.creator_id
+        WHERE title LIKE ? 
+           OR SOUNDEX(SUBSTRING_INDEX(title, ' ', 1)) = SOUNDEX(?)
+    `
+    const [result] = await db.query(sql, [`%${title}%`, title])
 
     return result
 }
 
 
 async function allgames() {
-    const sql = "SELECT title , description , banner_pic , creators.creator_pfp , creators.creator_name FROM games INNER JOIN creators ON games.creator_id = creators.creator_id "
+    const sql = "SELECT title , description , banner_pic , creators.creator_pfp , creators.creator_name FROM games INNER JOIN creators ON games.creator_id = creators.creator_id ORDER BY RAND() "
     const [result] = await db.query(sql)
     return result
 }
