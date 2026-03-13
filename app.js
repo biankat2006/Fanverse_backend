d7142c9ae36922f133197bcceb27a2ad55c7c432
diak <diak@mesterlevel.lo>
2026-03-13 08:12:04 +0100

    képek és képkezelés

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const path = require('path')
 const userRoutes = require('./routes/userRoutes.js')
 const mainRoutes = require('./routes/mainRoutes.js')
 
@@ -10,6 +11,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin:['http://localhost:5173'],credentials:'include'}))
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use('/creator', express.static(path.join(__dirname, 'creator')))
 
 app.use('/users/', userRoutes)
 app.use('/main/', mainRoutes )
